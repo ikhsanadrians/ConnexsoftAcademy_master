fe550470c5095aaae8d8e8c8574e0a2476151972
ikhsanadrians <aikhsan032@gmail.com>
2023-02-06 02:08:50 +0700

    mass

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Connexsoft Academy</title>
+    <title>Connexsoft Academy | Belajar Coding Mudah</title>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <link rel="stylesheet" href="{{ asset('css/base.css') }}">
