7d0cb18fe879d077280e8473bb93cbf35d813181
ikhsanadrians <aikhsan032@gmail.com>
2023-03-05 20:14:08 +0700

    add quiz js

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,12 +1,19 @@
 let allAnswer =  $('.answer');
 
 const correctAnswer = "answer-1"
+const unCorrectAnswer = true;
 
 
 for (const answer of allAnswer) {
     answer.addEventListener('click',function(){
-        if(answer.id === correctAnswer) answer.style.backgroundColor = "green"
-        else answer.style.backgroundColor = "red"
+        if(answer.id === correctAnswer){
+            answer.style.backgroundColor = "green"
+        }
+        else{
+            answer.style.backgroundColor = "red"
+        }
         console.log(answer.id)
     })
 }
+
+
